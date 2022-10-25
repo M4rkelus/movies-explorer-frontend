1be7760f58b8c9e225262fddfda19e1faa45547e
Mark Pirogov <m4rkpirogov@gmail.com>
2022-10-25 21:43:28 +0300

    fix: change base url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 /* APIs */
-// export const BASE_URL = "https://api.movies-explorer-mrk.nomorepartiesxyz.ru";
-export const BASE_URL = "http://localhost:3000"; //TODO Local
+// export const BASE_URL = "http://localhost:3000"; // Local
+export const BASE_URL = "https://api.movies-explorer-mrk.nomorepartiesxyz.ru";
 export const MOVIE_API_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 
 export const SHORT_MOVIE_DURATION = 40;
